22e79ad93eddec5f5dab189ee5156fc569c445ae
AdamTogether <adam.stewart888@gmail.com>
2018-04-25 00:07:36 -0700

    wiki--
    Removed "wiki" folder.

--- wiki/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Uncomment the following line to turn errors on
-//ini_set("display_errors","on");
-
-// This is needed to allow secure ldap authentication
-putenv('LDAPTLS_REQCERT=never');
-
-include('/nfs/ca/info/www_secure/wiki/pmwiki/pmwiki.php');
